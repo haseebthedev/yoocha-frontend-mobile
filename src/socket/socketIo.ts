d42ed4d4f97ba41b0755fb2f0eb4c5c1f79f7695
haseebthedev <haseebhowto@gmail.com>
2024-02-09 01:37:16 +0500

    added auth in socket communication

--- src/socket/socketIo.ts
+++ src/socket/socketIo.ts
@@ -1,10 +1,19 @@
 import { API_URL } from "config/config.dev";
 import { Socket, io } from "socket.io-client";
+import { loadString } from "utils/storage";
 
 export var socket: Socket | null = null;
 
-export const initSocketIO = () => {
-  socket = io(API_URL);
+export const initSocketIO = async () => {
+  const token = await loadString("UserToken");
+
+  let extraHeaders = {};
+
+  if (token) {
+    extraHeaders["authorization"] = token ? token : null;
+  }
+
+  socket = io(API_URL, { extraHeaders });
 };
 
 export const disconnectSocketIO = () => {
